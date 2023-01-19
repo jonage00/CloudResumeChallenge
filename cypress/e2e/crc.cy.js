226747d5bd1d387f35777be698eca6ba276ef0c9
jonage00 <jonathan.mark.martinez@gmail.com>
2023-01-18 20:32:36 -0800

    Created workflows

---
+++ cypress/e2e/crc.cy.js
@@ -0,0 +1,19 @@
+describe('Visitor Counter Test', () => {
+  it('increments after refresh', () => {
+    let initialCount;
+    cy.request('https://resumecountercrc-python.azurewebsites.net/api/VisitorCounter')
+      .then((response) => {
+        initialCount = parseInt(response.body);
+        expect(initialCount).to.be.a('number');
+      });
+
+    cy.reload();
+
+    cy.request('https://resumecountercrc-python.azurewebsites.net/api/VisitorCounter')
+      .then((response) => {
+        const updatedCount = parseInt(response.body);
+        expect(updatedCount).to.be.greaterThan(initialCount);
+      });
+  });
+});
+
