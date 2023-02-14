aa176eaed6032c3faa216be494a9cc5bb0fe41e0
jonage00 <jonathan.mark.martinez@gmail.com>
2023-02-14 15:51:30 -0800

    Updates to cypress test.

--- cypress/e2e/crc.cy.js
+++ cypress/e2e/crc.cy.js
@@ -1,7 +1,7 @@
 describe('Visitor Counter Test', () => {
   it('increments after refresh', () => {
     let initialCount;
-    cy.request('https://resumecountercrc-python.azurewebsites.net/api/VisitorCounter')
+    cy.request('https://countercrc-python.azurewebsites.net/api/VisitorCounter')
       .then((response) => {
         initialCount = parseInt(response.body);
         expect(initialCount).to.be.a('number');
@@ -9,7 +9,7 @@ describe('Visitor Counter Test', () => {
 
     cy.reload();
 
-    cy.request('https://resumecountercrc-python.azurewebsites.net/api/VisitorCounter')
+    cy.request('https://countercrc-python.azurewebsites.net/api/VisitorCounter')
       .then((response) => {
         const updatedCount = parseInt(response.body);
         expect(updatedCount).to.equal(initialCount + 1);
